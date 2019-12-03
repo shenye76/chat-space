620854261b94a9aee98c3a0b1b87c48f875ef744
shenye76 <shinyainoue76@gmail.com>
2019-12-03 15:22:35 +0900

    custom_data_propaty

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,26 +1,31 @@
 $(function(){
-
+  
   function buildHTML(message){
+    if (message.content) {
+      var conte = `<p class="lower-message__content">
+                     ${message.content}
+                   </p>`
+    } else {
+      var conte = ``
+    }
     if (message.image) {
       var img = `<img src=${message.image} >`
     } else {
       var img = ``
     }
-    var html = `<div class="message" data-message-id=${message.id}>
+    var html = `<div class="message" data-message_id=${message.id}>
                   <div class="upper-message">
                     <div class="upper-message__user-name">
                       ${message.user_name}
                     </div>
                     <div class="upper-message__date">
-                      ${message.date}
+                      ${message.created_at}
                     </div>
                   </div>
                   <div class="lower-message">
-                    <p class="lower-message__content">
-                      ${message.content}
-                    </p>
+                    ${conte}
+                    ${img}
                   </div>
-                  ${img}
                 </div>`
     return html;
   };
@@ -49,4 +54,24 @@ $(function(){
     });
     return false;
   })
+
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    last_message_id = $(".message:last").data('message_id');
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: '/groups/group_id/api/messages',
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      console.log('success');
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
 });
